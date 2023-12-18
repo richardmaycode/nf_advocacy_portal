0abb85191ed6681d813294d1ebeb0973aa96bacc
Richard Wise <richard.wise@hey.com>
2023-12-18 11:17:32 -0500

    begin admin routes and views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,14 +1,29 @@
 class Admin::UsersController < ApplicationController
   layout "dashboard"
+
+  before_action :set_user, only: %i[show edit update destroy]
   def index
+    @users = User.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @user = User.new
   end
 
-  def edit
+  def edit; end
+
+  def update; end
+
+  def destory
+    @user.destroy
+    redirect_to %i[admin users]
+  end
+
+  private
+
+  def set_user
+    @user = params[:id]
   end
 end
